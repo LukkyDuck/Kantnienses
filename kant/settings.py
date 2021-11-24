6333fe278e2c2f3b31a2f4bc5d134f746f369d93
LukkyDuck <77422744+LukkyDuck@users.noreply.github.com>
2021-11-24 01:29:21 -0300

    Update settings.py

--- kant/settings.py
+++ kant/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d(b+2ytd&jmsfv%a%zhicp^fw4p7k+bl*4u*)8c)a(i16n-n^b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
